e67e4efb62d4a72b9082cb3be4a905f73caf3abc
Math49 <mthsmercier@gmail.com>
2025-03-13 20:24:29 +0100

    Finalisation du mobile layout

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,12 +1,57 @@
 "use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthProvider";
+
 
 export default function NavMenu() {
 
+  const { logout } = useAuth();
+
+
+  const router = useRouter();
+
+  if (router.pathname === "/login") {
+    return null;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center C-text-black w-full">
-      <div className="flex flex-col">
-        <p className="text-2xl font-bold text-center">Relay Réassort</p>
-        <div className="C-bg-red h-[5px] w-full"></div>
+    <div className="flex flex-col items-center justify-between C-text-black h-full w-full">
+      <div className="flex flex-col items-center C-text-black w-full">
+        <div className="flex flex-col">
+          <p className="text-2xl font-bold text-center">Relay Réassort</p>
+          <div className="C-bg-red h-[5px] w-full"></div>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full mt-10 gap-8">
+          <div className="flex items-center justify-start w-full gap-10 navLink">
+            <div className={`h-[3vh] w-[5px] ${router.pathname === "/stock" ? "C-bg-red" : ""} `} ></div>
+            <Link href="/stock" className="font-bold text-xl">
+              Stocks
+            </Link>
+          </div>
+          <div className="flex items-center justify-start w-full gap-10 navLink">
+            <div className={`h-[3vh] w-[5px] ${router.pathname === "/listes" ? "C-bg-red" : ""} `}></div>
+            <Link href="/listes" className="font-bold text-xl">
+              Listes
+            </Link>
+          </div>
+          <div className="flex items-center justify-start w-full gap-10 navLink">
+            <div className={`h-[3vh] w-[5px] ${router.pathname === "/configuration" ? "C-bg-red" : ""} `}></div>
+            <Link href="/configuration" className="font-bold text-xl">
+              Configuration
+            </Link>
+          </div>
+          <div className="flex items-center justify-start w-full gap-10 navLink">
+            <div className={`h-[3vh] w-[5px] ${router.pathname === "/a-propos" ? "C-bg-red" : ""} `}></div>
+            <Link href="/a-propos" className="font-bold text-xl">
+              A propos
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col pb-5">
+        <p className="font-bold text-center cursor-pointer" onClick={logout}>Déconnexion</p>
       </div>
     </div>
   );
--- src/layouts/MobileLayout.jsx
+++ src/layouts/MobileLayout.jsx
@@ -6,7 +6,7 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Modal, ModalContent } from "@heroui/react"; // ✅ Vérification des bons imports
 
-export default function MobileLayout({ children, title }) {
+export default function MobileLayout({ children, title, city }) {
   const { user, logout } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -33,13 +33,17 @@ export default function MobileLayout({ children, title }) {
             >
               <ModalContent
                 as={motion.div}
-                className="bg-white fixed rounded-r-[40px] top-0 left-0 w-[70%] h-full shadow-lg pt-[5vh]"
+                className="bg-white fixed rounded-r-[40px] top-0 left-0 w-[70%] h-full shadow-lg pt-[5vh] overflow-hidden"
                 initial={{ x: "-100%" }}
                 animate={{ x: 0 }}
                 exit={{ x: "-100%" }}
                 transition={{ duration: 0.3, ease: "easeOut" }}
               >
                 <NavMenu />
+                <div className=" flex items-center justify-between px-[5vw] C-bg-red h-[10vh]">
+                  <img src="/images/elements/logo-relay.svg" />
+                  <p className="C-text-white font-bold text-2xl">Relay {city}</p>
+                </div>
               </ModalContent>
             </Modal>
           )}
