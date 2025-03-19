29e82ae44ad50683e1fe0605a5d6f01d3853e0c5
Math49 <mthsmercier@gmail.com>
2025-03-20 00:26:14 +0100

    page stocks #5

--- src/app/stocks/page.jsx
+++ src/app/stocks/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import BackButton from "@/components/BackButton";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getStocks } from "@/services/stock";
+import { getCategories } from "@/services/category";
 import { useAuth } from "@/context/AuthProvider";
 
 export default function HomePage() {
@@ -25,10 +27,26 @@ export default function HomePage() {
         fetchData();
       }, [user]);
 
+    
+    const colors = ["#FF6384", "#36A2EB", "#FFCE56", "#4CAF50", "#9C27B0"];
+
     return (
         <div className="flex flex-col gap-[5vh] items-center justify-center relative z-10 text-white w-[100%] p-5">
             <div className="w-[100%]">
                 <BackButton path="/" />
+            </div>
+            <div className="flex items-center justify-start gap-5 w-[100%] p-4 overflow-x-scroll">
+                {categories.map((category, index) => (
+                    <div
+                    key={index}
+                    className="p-4 rounded-[20px] shadow-md bg-white flex flex-col cursor-pointer gap-5 items-center justify-center border-t-[5px]"
+                    style={{ borderColor: colors[index % colors.length] }}
+                  >
+                    <p className="text-xl font-bold C-text-black">
+                      {category.Label}
+                    </p>
+                  </div>
+                ))}
             </div>
         </div>
     )
